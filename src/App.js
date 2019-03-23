adc81fa6f30104438a803f593e713bb939ee2d77
tonny <ton_kriddanai@hotmail.com>
2019-03-23 15:03:55 +0700

    Format document

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route} from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import 'react-bulma-components/dist/react-bulma-components.min.css';
+import "react-bulma-components/dist/react-bulma-components.min.css";
 
-import Homepage from './Home/pages/Homepage'
-import SelectMethodSelect from './Scrape/pages/SelectMethodSelect'
-import PrimaryKeyScrape from './Scrape/pages/PrimaryKeyScrape'
-
+import Homepage from "./Home/pages/Homepage";
+import SelectMethodSelect from "./Scrape/pages/SelectMethodSelect";
+import PrimaryKeyScrape from "./Scrape/pages/PrimaryKeyScrape";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/scrape/:repositoryName" component={SelectMethodSelect} />
-          <Route exact path="/scrape-pk/:repositoryName" component={PrimaryKeyScrape} />
-        </div>
-      </BrowserRouter>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <BrowserRouter basename={process.env.PUBLIC_URL}>
+                    <div>
+                        <Route exact path="/" component={Homepage} />
+                        <Route
+                            exact
+                            path="/scrape/:repositoryName"
+                            component={SelectMethodSelect}
+                        />
+                        <Route
+                            exact
+                            path="/scrape-pk/:repositoryName"
+                            component={PrimaryKeyScrape}
+                        />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
-
