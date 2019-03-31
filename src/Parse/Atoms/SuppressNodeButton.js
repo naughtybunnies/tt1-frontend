646ad9220026020f89dbc020f4e2b287a5b7fbe9
tonny <ton_kriddanai@hotmail.com>
2019-03-31 18:54:26 +0700

    link button with ajax actions

--- src/Parse/Atoms/SuppressNodeButton.js
+++ src/Parse/Atoms/SuppressNodeButton.js
@@ -1,8 +1,34 @@
 import React from "react";
+import axios from 'axios'
 import { faCompressArrowsAlt } from "@fortawesome/free-solid-svg-icons";
 
 import BaseButton from "../Atoms/BaseButton";
 
+const handleCompress = (targetNode, repositoryName) => {
+    console.log("selected", targetNode);
+    axios
+        .post(
+            window.API_ENDPOINT +
+                "/repository/" +
+                repositoryName +
+                "/graph/delete/",
+            { node_id: targetNode }
+        )
+        .then(response => {
+            window.location.reload();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
 export default function SuppressNodeButton(props) {
-    return <BaseButton icon={faCompressArrowsAlt} tooltip="Compress selected node."/>;
+    return (
+        <BaseButton
+            icon={faCompressArrowsAlt}
+            tooltip="Compress selected node."
+            onClick={props.targetNode ? () => handleCompress(props.targetNode, props.repositoryName) : () => {alert("no selected node found")}}
+            isDisabled={props.targetNode ? false : true}
+        />
+    );
 }
